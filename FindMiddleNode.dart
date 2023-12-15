8f316934ad88cea6e1329a807e53d481f25411d2
AmalAltayar <130461763+AmalAltayar@users.noreply.github.com>
2023-12-15 20:37:39 +0300

    FindMiddleNode.dart

---
+++ FindMiddleNode.dart
@@ -0,0 +1,24 @@
+Node? findMiddleNode(Node? head) {
+  if (head == null) return null;
+
+  Node? slow = head;
+  Node? fast = head;
+
+  while (fast?.next != null && fast?.next?.next != null) {
+    slow = slow?.next;
+    fast = fast?.next?.next;
+  }
+
+  return slow;
+}
+
+void main() {
+  // Example linked list
+  Node head = Node(1);
+  head.next = Node(2);
+  head.next?.next = Node(3);
+  head.next?.next?.next = Node(4);
+
+  Node? middleNode = findMiddleNode(head);
+  print("Middle Node value: ${middleNode?.data}");
+}
